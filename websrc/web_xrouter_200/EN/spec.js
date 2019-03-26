5ca38f46860b8cd0bd4d7a025fdac34d2e6965f2
rfidspan <44983572+rfidspan@users.noreply.github.com>
2019-03-26 11:43:32 -0700

    Update spec.js

--- websrc/web_xrouter_200/EN/spec.js
+++ websrc/web_xrouter_200/EN/spec.js
@@ -20,9 +20,7 @@ var Pages=new Array(
 ,new Page(0,'sta setting','sta_config.html','STA Interface Setting')
 ,new Page(0,'app setting','app_config.html',' Application Setting')
 ,new Page(0,'device management ','management.html','Device Management')
-
-
-
+,new Page(0,'device constellation ','constellation.html','Device constellation')
 
 );
 
