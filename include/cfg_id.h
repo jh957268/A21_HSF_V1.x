f41c3c79451bdf2a2b68806430a4a0aa671a7993
rfidspan <44983572+rfidspan@users.noreply.github.com>
2019-06-29 23:03:19 -0700

    Update cfg_id.h

--- include/cfg_id.h
+++ include/cfg_id.h
@@ -164,13 +164,13 @@
 #define CFG_AKS_ADVA15			        CFG_USER_(62,CFG_TYPE_STR)
 #define CFG_AKS_ADVA16			        CFG_USER_(63,CFG_TYPE_STR)
 
-#define CFG_PROTNAME			        CFG_USER_(64,CFG_TYPE_INT)
-
 #define CFG_AKS_REL_BUILD				CFG_USER_(64,CFG_TYPE_STR)
 #define	CFG_SACN_OUTPUT					CFG_USER_(65,CFG_TYPE_STR)
 #define	CFG_SACN_UNIV					CFG_USER_(66,CFG_TYPE_STR)
 #define	CFG_LED_COLOR					CFG_USER_(67,CFG_TYPE_STR)
 #define CFG_BATTERY_ID					CFG_USER_(68,CFG_TYPE_STR)
+
+#define CFG_PROTNAME			        CFG_USER_(69,CFG_TYPE_INT)
 
 // M2M: gohuy
 #define CFG_M2M_(idx,type) 			CFG_ID_(CFG_ID_M2M,idx,type,0)
