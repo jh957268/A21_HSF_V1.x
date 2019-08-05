8a3dc6dfe6b54834a40b2ea11c43ac6ec4c312d4
rfidspan <44983572+rfidspan@users.noreply.github.com>
2019-08-05 00:15:51 -0700

    Update cgi_api.c

--- websrc/cgi/cgi_v2/cgi_api.c
+++ websrc/cgi/cgi_v2/cgi_api.c
@@ -876,7 +876,8 @@ void CGI_var_map(http_req *req, char *name, int id)
 			return;
 		case CFG_AKS_CONS_NUM:
 			ratpac_get_str(id, val);
-			WEB_printf(req, "var aks_num = '%s'",val);			// last one
+			WEB_printf(req, "consAddRow('%s');\n",val);			
+
 			break;
 		default:
 			idx = ratpac_get_str(id, val);
